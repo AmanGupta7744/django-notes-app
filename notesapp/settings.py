fc07faa64c35dec642407e8443d620b7f21769b9
AmanGupta <52156156+AmanGupta7744@users.noreply.github.com>
2025-09-17 17:16:44 +0530

    Update settings.py

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -87,6 +87,7 @@ WSGI_APPLICATION = 'notesapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+"""
 DATABASES = {
 
     'default': {
@@ -98,6 +99,15 @@ DATABASES = {
         'PORT': os.getenv("DB_PORT"),
     }
 }
+"""
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
 
 
 # Password validation
